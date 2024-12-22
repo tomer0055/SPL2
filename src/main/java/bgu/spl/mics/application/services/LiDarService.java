dfed9091853cca77cbbe112fd0ff018111eddfa0
Tomer Hamuel <tomerhamuel@Tomers-MacBook-Pro.local>
2024-12-22 15:08:11 +0200

    Initial commit

---
+++ src/main/java/bgu/spl/mics/application/services/LiDarService.java
@@ -0,0 +1,36 @@
+package bgu.spl.mics.application.services;
+
+
+import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.objects.LiDarWorkerTracker;
+
+/**
+ * LiDarService is responsible for processing data from the LiDAR sensor and
+ * sending TrackedObjectsEvents to the FusionSLAM service.
+ * 
+ * This service interacts with the LiDarTracker object to retrieve and process
+ * cloud point data and updates the system's StatisticalFolder upon sending its
+ * observations.
+ */
+public class LiDarService extends MicroService {
+
+    /**
+     * Constructor for LiDarService.
+     *
+     * @param liDarTracker The LiDAR tracker object that this service will use to process data.
+     */
+    public LiDarService(LiDarWorkerTracker liDarTracker) {
+        super("Change_This_Name");
+        // TODO Implement this
+    }
+
+    /**
+     * Initializes the LiDarService.
+     * Registers the service to handle DetectObjectsEvents and TickBroadcasts,
+     * and sets up the necessary callbacks for processing data.
+     */
+    @Override
+    protected void initialize() {
+        // TODO Implement this
+    }
+}
