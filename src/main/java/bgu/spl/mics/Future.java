0c472d0960ffce33021d0af899dde4387d7550ca
Tomer Hamuel <tomer0033265@gmail.com>
2024-12-25 17:31:50 +0200

    Update Future.java

--- src/main/java/bgu/spl/mics/Future.java
+++ src/main/java/bgu/spl/mics/Future.java
@@ -11,55 +11,58 @@ import java.util.concurrent.TimeUnit;
  * No public constructor is allowed except for the empty constructor.
  */
 public class Future<T> {
-	
+
 	/**
 	 * This should be the the only public constructor in this class.
 	 */
 	public Future() {
-		//TODO: implement this
+		// TODO: implement this
 	}
-	
+
 	/**
-     * retrieves the result the Future object holds if it has been resolved.
-     * This is a blocking method! It waits for the computation in case it has
-     * not been completed.
-     * <p>
-     * @return return the result of type T if it is available, if not wait until it is available.
-     * 	       
-     */
+	 * retrieves the result the Future object holds if it has been resolved.
+	 * This is a blocking method! It waits for the computation in case it has
+	 * not been completed.
+	 * <p>
+	 * 
+	 * @return return the result of type T if it is available, if not wait until it
+	 *         is available.
+	 * 
+	 */
 	public T get() {
-		//TODO: implement this.
+		// TODO: implement this.
 		return null;
 	}
-	
+
 	/**
-     * Resolves the result of this Future object.
-     */
-	public void resolve (T result) {
-		//TODO: implement this.
+	 * Resolves the result of this Future object.
+	 */
+	public void resolve(T result) {
+		// TODO: implement this.
 	}
-	
+
 	/**
-     * @return true if this object has been resolved, false otherwise
-     */
-	public boolean isDone() {
-		//TODO: implement this.
+	 * @return true if this object has been resolved, false otherwise
+	 */
+	public  boolean isDone() {
+		// TODO: implement this.
 		return false;
 	}
-	
+
 	/**
-     * retrieves the result the Future object holds if it has been resolved,
-     * This method is non-blocking, it has a limited amount of time determined
-     * by {@code timeout}
-     * <p>
-     * @param timout 	the maximal amount of time units to wait for the result.
-     * @param unit		the {@link TimeUnit} time units to wait.
-     * @return return the result of type T if it is available, if not, 
-     * 	       wait for {@code timeout} TimeUnits {@code unit}. If time has
-     *         elapsed, return null.
-     */
+	 * retrieves the result the Future object holds if it has been resolved,
+	 * This method is non-blocking, it has a limited amount of time determined
+	 * by {@code timeout}
+	 * <p>
+	 * 
+	 * @param timout the maximal amount of time units to wait for the result.
+	 * @param unit   the {@link TimeUnit} time units to wait.
+	 * @return return the result of type T if it is available, if not,
+	 *         wait for {@code timeout} TimeUnits {@code unit}. If time has
+	 *         elapsed, return null.
+	 */
 	public T get(long timeout, TimeUnit unit) {
-		//TODO: implement this.
+		// TODO: implement this.
 		return null;
 	}
 
