f48a41a8d1ae53421a9348e50765fc71534d10cb
TheMit23 <amittami@post.bgu.ac.il>
2024-12-25 10:02:13 +0200

    future

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,61 +1,81 @@
 package bgu.spl.mics;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
  * Write your implementation here!
  * Only private fields and methods can be added to this class.
  */
 public class MessageBusImpl implements MessageBus {
+	private static MessageBusImpl instance = null;
+	private ConcurrentHashMap<Class<? extends Event>, ConcurrentLinkedQueue<MicroService>> eventMap;
+	private ConcurrentHashMap<Class<? extends Broadcast>, ConcurrentLinkedQueue<MicroService>> broadcastMap;
+	private ConcurrentHashMap<MicroService,ConcurrentLinkedQueue<Message>> microServiceMap;
+	private ConcurrentHashMap<Event,Future> events;
+	private MessageBusImpl(){};
+
+	public static MessageBusImpl getInstance() {
+		if(instance==null){
+			instance = new MessageBusImpl();
+		}
+		return instance;
+	}
 
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		// TODO Auto-generated method stub
-
+		eventMap.putIfAbsent(type, new ConcurrentLinkedQueue<>());
 	}
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		// TODO Auto-generated method stub
-
+		broadcastMap.putIfAbsent(type, new ConcurrentLinkedQueue<>());
 	}
 
 	@Override
 	public <T> void complete(Event<T> e, T result) {
-		// TODO Auto-generated method stub
-
+		(events.get(e)).resolve(result);
 	}
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		// TODO Auto-generated method stub
-
+		ConcurrentLinkedQueue<MicroService> microServices = broadcastMap.get(b.getClass());
+		for(MicroService m : microServices){
+			microServiceMap.get(m).add(b);
+		}
 	}
 
 	
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
-		// TODO Auto-generated method stub
-		return null;
+		Future<T> future = new Future<>();
+		events.put(e,future);
+		ConcurrentLinkedQueue<MicroService> microServices = eventMap.get(e.getClass());
+		MicroService m = microServices.poll();
+		microServiceMap.get(m).add(e);
+		microServices.add(m);
+		return future;
 	}
 
 	@Override
 	public void register(MicroService m) {
-		// TODO Auto-generated method stub
-
+		microServiceMap.putIfAbsent(m,new ConcurrentLinkedQueue<>());
 	}
 
 	@Override
 	public void unregister(MicroService m) {
-		// TODO Auto-generated method stub
-
+		microServiceMap.remove(m);
+		for(ConcurrentLinkedQueue<MicroService> microServices : eventMap.values()){
+			microServices.remove(m);
+		}
+		for(ConcurrentLinkedQueue<MicroService> microServices : broadcastMap.values()){
+			microServices.remove(m);
+		}
 	}
 
 	@Override
 	public Message awaitMessage(MicroService m) throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
+		return microServiceMap.get(m).poll();
 	}
-
-	
-
 }
