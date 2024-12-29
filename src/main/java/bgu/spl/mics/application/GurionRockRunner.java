e4e2c4925b9af641183ed68703625affe791cde3
TheMit23 <amittami@post.bgu.ac.il>
2024-12-29 11:20:01 +0200

    main

--- src/main/java/bgu/spl/mics/application/GurionRockRunner.java
+++ src/main/java/bgu/spl/mics/application/GurionRockRunner.java
@@ -1,5 +1,15 @@
 package bgu.spl.mics.application;
 
+import bgu.spl.mics.MessageBusImpl;
+import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.objects.Camera;
+import bgu.spl.mics.application.objects.FusionSlam;
+import bgu.spl.mics.application.objects.GPSIMU;
+import bgu.spl.mics.application.services.CameraService;
+import bgu.spl.mics.application.services.FusionSlamService;
+import bgu.spl.mics.application.services.PoseService;
+import bgu.spl.mics.application.services.TimeService;
+
 /**
  * The main entry point for the GurionRock Pro Max Ultra Over 9000 simulation.
  * <p>
@@ -18,10 +28,18 @@ public class GurionRockRunner {
      * @param args Command-line arguments. The first argument is expected to be the path to the configuration file.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
         // TODO: Parse configuration file.
+        
+
         // TODO: Initialize system components and services.
+        MessageBusImpl messageBus = MessageBusImpl.getInstance();
+        MicroService timeService = new TimeService(100, 1000);
+        MicroService poseService = new PoseService(new GPSIMU());
+        MicroService cameraService = new CameraService(new Camera());
+        MicroService fusionSlamService = new FusionSlamService( FusionSlam.getInstance());
+
+
         // TODO: Start the simulation.
     }
 }
