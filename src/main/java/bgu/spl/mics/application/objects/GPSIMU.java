97df70ed06112ee15d726a4bcbf18c88807438ab
TheMit23 <amittami@post.bgu.ac.il>
2025-01-01 18:16:04 +0200

    hello tomer

--- src/main/java/bgu/spl/mics/application/objects/GPSIMU.java
+++ src/main/java/bgu/spl/mics/application/objects/GPSIMU.java
@@ -4,7 +4,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -16,20 +15,15 @@ import com.google.gson.reflect.TypeToken;
 public class GPSIMU {
     private int CurrentTick;
     private STATUS status;
-    private ConcurrentHashMap<Integer,Pose> PoseMap;
+    private List<Pose> PoseList;
     public GPSIMU(String filePath ){
         this.CurrentTick = 0;
         this.status = STATUS.UP;
         Gson gson = new Gson();
         try (FileReader reader = new FileReader(filePath)) {
-            List<Pose> PoseList;
             Type dataType = new TypeToken<List<Pose>>() {
             }.getType();
             PoseList = gson.fromJson(reader, dataType);
-            PoseMap = new ConcurrentHashMap<>();
-            for(Pose pose : PoseList){
-                PoseMap.put(pose.getTime(),pose);
-            }
         }
 
         catch (IOException e) {
