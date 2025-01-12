38fd0d9416d66430b9d7b79168f13aa1e56f9f76
TheMit23 <amittami@post.bgu.ac.il>
2025-01-12 20:23:52 +0200

    new

--- src/test/java/CameraTests.java
+++ src/test/java/CameraTests.java
@@ -5,38 +5,34 @@ import bgu.spl.mics.application.objects.Camera;
 import bgu.spl.mics.application.objects.StampedDetectedObjects;
 import bgu.spl.mics.application.objects.StatisticalFolder;
 
-
-
 public class CameraTests {
 
     private Camera camera;
-
 
     @Test
     public void setUp() {
         int id = 1;
         int ferequency = 1;
         StatisticalFolder folder = new StatisticalFolder();
-        String path= "src/test/java/resources/camera_data.json";
-        this.camera = new Camera(id, ferequency,"camera1", path);
+        String path = "src/test/java/resources/camera_data.json";
+        this.camera = new Camera(id, ferequency, "camera1", path);
         StampedDetectedObjects t = camera.getDetectedObjectsByTime(2);
-        System.out.println("result --->>       "+t.toString());
+        System.out.println("result --->>       " + t.toString());
         assertNotNull(t);
-        
+
     }
+
     @Test
     public void setUpWithErrors() {
         int id = 1;
         int ferequency = 1;
         StatisticalFolder folder = new StatisticalFolder();
-        String path= "src/test/java/resources/camera_data.json";
-        this.camera = new Camera(id, ferequency,"camera1", path);
+        String path = "src/test/java/resources/camera_data.json";
+        this.camera = new Camera(id, ferequency, "camera1", path);
         StampedDetectedObjects t = camera.getDetectedObjectsByTime(14);
-        System.out.println("result --->>       "+t.toString());
+        System.out.println("result --->>       " + t.toString());
         assertNotNull(t);
-        
+
     }
 
-
-    
 }
